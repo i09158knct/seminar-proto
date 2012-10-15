cd93428eefbc8f6ad0f4fa91908271fcc5eb908f
i09158knct <i09158knct@gmail.com>
2012-10-15 19:06:02 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeminarProto::Application.config.secret_token = 'c36306e7072729030c75880f28095957f0bc3fca7a7db8cc1104d9107404a7798e4c73401906a0c104d91f828479959c54a0580acfec10ac0a969e10995bd87c'
