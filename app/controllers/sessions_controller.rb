56f213a6849231399eccb4ffebec3f2b9a47b054
i09158knct <i09158knct@gmail.com>
2012-10-16 14:05:40 +0900

    fix logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
            User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "You have logged in successfully."
+    redirect_to root_url, :notice => "Logged in!"
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "You have logged out successfully."
+    reset_session
+    redirect_to root_url, :notice => "Logged out!"
   end
 end
