72d3437b650916136160c3c161d35e77b77ea1c4
i09158knct <i09158knct@gmail.com>
2012-10-16 14:06:14 +0900

    add some helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  helper_method :authenticate_user!
 
   private
-
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def user_signed_in?
+      return true if current_user
+    end
+
+    def authenticate_user!
+      if !current_user
+        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+      end
     end
 end
